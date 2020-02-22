2a3a21679e363562f346bfafcd6e9b41419dd93e
Valeria Khristinina <valeria.khristinina@gmail.com>
2020-02-22 23:01:03 +0100

    first commit

---
+++ HW2.8/LanguagesDatamanager.swift
@@ -0,0 +1,14 @@
+//
+//  LanguagesDatamanager.swift
+//  HW2.8
+//
+//  Created by Valeria Khristinina on 22/02/2020.
+//  Copyright © 2020 VIA. All rights reserved.
+//
+
+import Foundation
+
+
+class LanguagesDataManager {
+	
+}
