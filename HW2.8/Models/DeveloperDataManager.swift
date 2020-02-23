7098a7ccdf58198efd6e396e63ec0f3a81b6ce17
Inna Lapteva <innalapteva72@gmail.com>
2020-02-23 14:24:11 +0500

    added developerDataModel

---
+++ HW2.8/Models/DeveloperDataManager.swift
@@ -0,0 +1,22 @@
+//
+//  DeveloperDataManager.swift
+//  HW2.8
+//
+//  Created by Инна Лаптева on 23.02.2020.
+//  Copyright © 2020 VIA. All rights reserved.
+//
+
+import Foundation
+
+class DeveloperDataManager {
+    
+    static let shared = DeveloperDataManager()
+    
+    var names = ["Inna", "Alexey", "Valeria"]
+    
+    var surnames = ["Lapteva", "Khristinina", "Alexey"]
+    
+    var avatarImage = ["745396026", "145036843", "287185656"]
+    
+}
+
